0d04ed330d98ec48326b03d9106c7c13a7f77cea
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-03-26 12:47:07 +0700

    tugas hari selasa

---
+++ selasa/cariPelaku.js
@@ -0,0 +1,11 @@
+function cariPelaku(str){
+    var cariabc = str.match(/abc/g) 
+    return cariabc.length
+}
+  
+  // TEST CASES
+  console.log(cariPelaku('mabcvabc')); // 2
+  console.log(cariPelaku('abcdabdc')); // 1
+  console.log(cariPelaku('bcabcac')); // 1
+  console.log(cariPelaku('abcabcabc')); // 3
+  console.log(cariPelaku('babcbacabc')); // 2
